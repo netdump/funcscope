b4e6632fdb1837e093dde68aaa09662e87e208f7
nothing <nothing163mail@163.com>
2026-01-14 22:00:55 +0800

    [add] four

--- tool.c
+++ tool.c
@@ -1,1 +1,28 @@
 
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/un.h>
+#include <sys/uio.h>
+#include <sys/vfs.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "funcscope.h"
+
+
+
+int main(void) {
+
+
+
+    return 0;
+}
